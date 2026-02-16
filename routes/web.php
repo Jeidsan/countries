3f54f3ed28586e10a85eb74034c914064457418f
Jeidsan A. da C. Pereira <jeidsan.pereira@gmail.com>
2026-02-15 21:56:46 -0300

    Adicionando assets e dados

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function(){
+    echo 'Hello world!';
+});
+
+Route::get('/show-data', [MainController::class, 'showData']);
